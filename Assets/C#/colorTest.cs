29737573d65c86dae3571b447a5949294ddb5208
siqihuang <siqihuang2014@gmail.com>
2015-04-19 20:14:29 -0400

    map
    map

---
+++ Assets/C#/colorTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorTest : MonoBehaviour {
+	public Material yellow,green;
+	// Use this for initialization
+	void Start () {
+		renderer.material = yellow;
+	}
+	
+	// Update is called once per frame
+	void Update(){
+	}
+
+	void OnMouseDown(){
+		renderer.material = green;
+		//renderer.material.color.a = 0.5f;
+	}
+}
